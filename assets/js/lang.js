b990f805a4253b72f230e5637667dcbf6cce216b
paterson3c <miguel.paterson@estudiante.uam.es>
2025-04-11 10:11:46 +0200

    v3.1 fixing

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -12,7 +12,6 @@ function setLang(lang) {
   const flag = document.getElementById('current-flag');
   if (flag) flag.src = `${prefix}assets/img/flags/${lang}.svg`;
 }
-
 function loadLang(lang) {
   const prefix = window.location.pathname.includes("/projects/") ? "../" : "";
   fetch(`${prefix}assets/lang/${lang}.json`)
@@ -20,11 +19,10 @@ function loadLang(lang) {
     .then(data => {
       document.querySelectorAll('[data-i18n]').forEach(el => {
         const key = el.getAttribute('data-i18n');
-        const translation = key.split('.').reduce((obj, part) => obj?.[part], data);
+        const translation = key.split('.').reduce((o, i) => (o ? o[i] : null), data);
         if (translation) el.innerText = translation;
       });
-    })
-    .catch(err => console.error("Translation loading failed:", err));
+    });
 }
 
 window.addEventListener('DOMContentLoaded', () => {
